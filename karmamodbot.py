082aee00751c053dd619f07c026c25fb7c9885e2
Brian Herman <brianherman@gmail.com>
2014-05-09 17:14:44 -0500

    added config option.

--- karmamodbot.py
+++ karmamodbot.py
@@ -2,31 +2,44 @@ import time
 import praw
 import re
 import requests
-from pprint import pprint
-r = praw.Reddit(user_agent='')
-r.login()
+import yaml
+
+#grab stuff from config file
+config_file = open('config.yaml')
+yaml        = yaml.safe_load(config_file)
+username    = yaml['username']
+password    = yaml['password']
+subreddit_to_mod   = yaml['subreddit']
+reply       = yaml['disclaimer']
+
+reply = reply.replace("{{subreddit}}",subreddit_to_mod) 
+
+r = praw.Reddit(user_agent=username)
+
+r.login(username, password)
 
 if r.is_logged_in():
     print "logged in"
+
 already_done = []
-def get_soup(url):
+
+def check_kd(url):
     user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
     headers = { 'User-Agent' : user_agent }
     request = requests.get(url, headers=headers)
     print request.text
     return request.text 
 
-
 while True:
-    subreddit = r.get_subreddit('test_2')
+    subreddit = r.get_subreddit(subreddit_to_mod)
     for submission in subreddit.get_new(limit=10): 
-        print submission
-        kd = get_soup('http://karmadecay.com/r/test_2/comments/'+submission.id)
+        kd = check_kd('http://karmadecay.com/r/'+subreddit_to_mod+'/comments/'+submission.id)
         mul = re.compile("Found [0-9] very similar images")
         one = re.compile("Found [0-9] very similar image")
         if mul.search(kd) or one.search(kd):
             if submission.id not in already_done:
                 print submission.id
+                submission.add_comment(reply)
                 submission.remove(False)
                 already_done.append(submission.id) 
     time.sleep(1800)
